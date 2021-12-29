d5122a8094006f54fe4646df16c5b063f71d3c56
mdrijwan <mrrmmonty@gmail.com>
2021-12-30 05:44:53 +0800

    Adding listAvailability function

--- src/api/list.ts
+++ src/api/list.ts
@@ -1,5 +1,5 @@
 import { formatResponse } from '../helpers/formatter'
-import { scanData, scanTable } from '../helpers/common'
+import { queryAvailability, queryDate, scanData, scanTable } from '../helpers/common'
 
 export const candidates = async () => {
   try {
@@ -18,6 +18,18 @@ export const appointments = async (event) => {
     const appointmentsData = await scanData(id)
     console.log('DATA', appointmentsData)
     return formatResponse(200, appointmentsData)
+  } catch (error) {
+    console.log('ERROR', error)
+    return formatResponse(400, error)
+  }
+}
+
+export const availability = async (event) => {
+  try {
+    const date = event.queryStringParameters ? event.queryStringParameters.date : null
+    const availabilityData = date != null ? await queryDate(date) : await queryAvailability()
+    console.log('DATA', availabilityData)
+    return formatResponse(200, availabilityData)
   } catch (error) {
     console.log('ERROR', error)
     return formatResponse(400, error)
